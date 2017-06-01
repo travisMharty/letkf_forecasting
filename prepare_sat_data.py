3603ca075b11ff505e432b9d010ef2e222a6947f
travisMharty <travisharty@math.arizona.edu>
2017-06-01 12:21:44 -0700

    added function to find and sort sat files

--- prepare_sat_data.py
+++ prepare_sat_data.py
@@ -3,16 +3,39 @@ import pandas as pd
 import xarray as xr
 from glob import glob
 
-def get_files(start_day, end_day, loc, max_zenith=60):
-    start_day_of_year = start_day.dayofyear
-    end_day_of_year = end_day.dayofyear
+def get_all_files(start_hour=11, end_hour=3):
+    """
+    start_hour - in UTC
+    end_hour - in UTC"""
+    if end_hour < start_hour:
+        hour_range = np.union1d(np.arange(start_hour, 23 + 1),
+                                np.arange(0, end_hour + 1))
+    else:
+        hour_range = np.arange(start_hour, end_hour)
+    files = []
+    for hour in hour_range:
+        temp = glob(
+            ('/a2/uaren/goes_images/april/goes15.2014.*.{hour:02d}*.BAND_01.nc'
+                          .format(hour=hour)))
+        files = files + temp
+        temp = glob(
+            ('/a2/uaren/goes_images/may/goes15.2014.*.{hour:02d}*.BAND_01.nc'
+                          .format(hour=hour)))
+        files = files + temp
+        temp = glob(
+            ('/a2/uaren/goes_images/june/goes15.2014.*.{hour:02d}*.BAND_01.nc'
+                          .format(hour=hour)))
+        files = files + temp
+    files.sort(key = lambda x: x.sort('.2014.')[1])
+    return files
 
 
 def sphere_to_lcc(self, lats, lons, R=6370, truelat0=31.7, truelat1=31.7,
                   ref_lat=31.68858, stand_lon=-113.7):
     """
     Taken from Tony Lorenzo's repository at:
-    https://github.com/alorenzo175/satellite_irradiance_optimal_interpolation.git.
+    https://github.com/alorenzo175/
+         satellite_irradiance_optimal_interpolation.git.
     Convert from spherical lats/lons like what comes out of WRF to the WRF
     Lambert Conformal x/y coordinates. Defaults are what
     are generally used for the AZ domain
@@ -43,7 +66,8 @@ def lcc_to_sphere(self, x, y, R=6370, truelat0=31.7, truelat1=31.7,
                   ref_lat=31.68858, stand_lon=-113.7):
     """
     Taken from Tony Lorenzo's repository at:
-    https://github.com/alorenzo175/satellite_irradiance_optimal_interpolation.git.
+    https://github.com/alorenzo175/
+         satellite_irradiance_optimal_interpolation.git.
     Convert from spherical lats/lons like what comes out of WRF to the WRF
     Lambert Conformal x/y coordinates. Defaults are what
     are generally used for the AZ domain
