5c23c2f050a8e656242b418ac3f63620488c0fea
travisMharty <travisharty@math.arizona.edu>
2017-09-25 17:30:19 -0700

    Made faster by Calculating rh after selecting time and fixed an issue with using a float instead on an int in .sel

--- get_wrf_data.py
+++ get_wrf_data.py
@@ -45,6 +45,17 @@ def main(time_range, wrf_path, save_path):
     file_path: location of wrf dataset
     '''
     dataset = xr.open_dataset(wrf_path)
+    data_times = dataset['Times'].to_pandas().apply(
+        lambda x: pd.to_datetime(x.decode("utf-8").replace('_', ' ')))
+    data_times = pd.Index(data_times).tz_localize('UTC').tz_convert('MST')
+    start_time = time_range[0]
+    end_time = time_range[-1]
+    times_int = dataset.Time.values
+    times_int = times_int[(data_times >= start_time) & (data_times <= end_time)]
+    data_times = data_times[times_int]
+    num_of_times = times_int.size
+    dataset = dataset.sel(Time=times_int)
+    times_int = dataset.Time.values
     dataset['pressure'] = dataset['P'] + dataset['PB']
     dataset['temp'] = (dataset['T']+300)*(dataset['pressure']/100000)**0.2854
     dataset['rh'] = rh_calc(
@@ -53,25 +64,17 @@ def main(time_range, wrf_path, save_path):
     dims.remove('Time')
     dims.remove('bottom_top')
     dataset['average_rh'] = dataset['rh'].mean(dim=dims)
-    data_times = dataset['Times'].to_pandas().apply(
-        lambda x: pd.to_datetime(x.decode("utf-8").replace('_', ' ')))
-    data_times = pd.Index(data_times).tz_localize('UTC').tz_convert('MST')
-    start_time = time_range[0]
-    end_time = time_range[-1]
-    times_int = dataset.Time.values
-    times_int = times_int[(data_times >= start_time) & (data_times <= end_time)]
-    num_of_times = times_int.size
     temp = dataset.sel(Time=times_int[0])
     bottom_top = pd.Series()
     bottom_top[data_times[0]] = temp['bottom_top'].where(
         temp['average_rh'] == temp['average_rh'].max(), drop=True).item()
     U = pd.DataFrame(
         data=temp['U'].sel(
-            bottom_top=bottom_top[data_times[0]]).values.ravel()[None, :],
+            bottom_top=int(bottom_top[data_times[0]])).values.ravel()[None, :],
         index=[data_times[0]])
     V = pd.DataFrame(
         data=temp['V'].sel(
-            bottom_top=bottom_top[data_times[0]]).values.ravel()[None, :],
+            bottom_top=int(bottom_top[data_times[0]])).values.ravel()[None, :],
         index=[data_times[0]])
     for t in np.arange(num_of_times - 1) + 1:
         temp = dataset.isel(Time=t)
@@ -79,11 +82,13 @@ def main(time_range, wrf_path, save_path):
             temp['average_rh'] == temp['average_rh'].max(), drop=True).item()
         U_temp = pd.DataFrame(
             data=temp['U'].sel(
-                bottom_top=bottom_top[data_times[t]]).values.ravel()[None, :],
+                bottom_top=int(
+                    bottom_top[data_times[t]])).values.ravel()[None, :],
             index=[data_times[t]])
         V_temp = pd.DataFrame(
             data=temp['V'].sel(
-                bottom_top=bottom_top[data_times[t]]).values.ravel()[None, :],
+                bottom_top=int(
+                    bottom_top[data_times[t]])).values.ravel()[None, :],
             index=[data_times[t]])
         U = U.append(U_temp)
         V = V.append(V_temp)
