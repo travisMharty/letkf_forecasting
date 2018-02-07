b20595bcfe51db50996e9376e09d0b45f33143bb
travisMharty <travisharty@math.arizona.edu>
2018-02-07 15:19:04 -0700

    Commented out numba stuff. Do not want to delete it yet, but did not install numba in my main env.

--- letkf_forecasting.py
+++ letkf_forecasting.py
@@ -19,9 +19,8 @@ import prepare_sat_data as prep
 
 a = 6371000  # average radius of earth when modeled as a sphere From wikipedia
 
-from numba import jit
+# from numba import jit
 
-@jit
 def time_deriv_3(q, dt, u, dx, v, dy):
     k = space_deriv_4(q, u, dx, v, dy)
     k = space_deriv_4(q + dt/3*k, u, dx, v, dy)
@@ -30,15 +29,15 @@ def time_deriv_3(q, dt, u, dx, v, dy):
     return qout
 
 
-@jit(nopython=True)
-def time_deriv_3_numba(q, dt, u, dx, v, dy):
-    k = space_deriv_4_numba(q, u, dx, v, dy)
-    k = space_deriv_4_numba(q + dt/3*k, u, dx, v, dy)
-    k = space_deriv_4_numba(q + dt/2*k, u, dx, v, dy)
-    qout = q + dt*k
-    return qout
+# @jit(nopython=True)
+# def time_deriv_3_numba(q, dt, u, dx, v, dy):
+#     k = space_deriv_4_numba(q, u, dx, v, dy)
+#     k = space_deriv_4_numba(q + dt/3*k, u, dx, v, dy)
+#     k = space_deriv_4_numba(q + dt/2*k, u, dx, v, dy)
+#     qout = q + dt*k
+#     return qout
 
-@jit
+
 def space_deriv_4(q, u, dx, v, dy):
     qout = np.zeros_like(q)
     F_x = np.zeros_like(u)
@@ -123,117 +122,117 @@ def space_deriv_4(q, u, dx, v, dy):
     return qout
 
 
-### Numba test
+# ### Numba test
 
-@jit(nopython=True)
-def space_deriv_4_numba(q, u, dx, v, dy):
-    qout = np.zeros_like(q)
-    F_x = np.zeros_like(u)
-    F_y = np.zeros_like(v)
+# @jit(nopython=True)
+# def space_deriv_4_numba(q, u, dx, v, dy):
+#     qout = np.zeros_like(q)
+#     F_x = np.zeros_like(u)
+#     F_y = np.zeros_like(v)
 
-    # middle calculation
-    F_x[:, 2:-2] = u[:, 2:-2]/12*(
-        7*(q[:, 2:-1] + q[:, 1:-2]) - (q[:, 3:] + q[:, :-3]))
-    F_y[2:-2, :] = v[2:-2, :]/12*(
-        7*(q[2:-1, :] + q[1:-2, :]) - (q[3:, :] + q[:-3, :]))
-    qout[:, 2:-2] = qout[:, 2:-2] - (F_x[:, 3:-2] - F_x[:, 2:-3])/dx
-    qout[2:-2, :] = qout[2:-2, :] - (F_y[3:-2, :] - F_y[2:-3, :])/dy
+#     # middle calculation
+#     F_x[:, 2:-2] = u[:, 2:-2]/12*(
+#         7*(q[:, 2:-1] + q[:, 1:-2]) - (q[:, 3:] + q[:, :-3]))
+#     F_y[2:-2, :] = v[2:-2, :]/12*(
+#         7*(q[2:-1, :] + q[1:-2, :]) - (q[3:, :] + q[:-3, :]))
+#     qout[:, 2:-2] = qout[:, 2:-2] - (F_x[:, 3:-2] - F_x[:, 2:-3])/dx
+#     qout[2:-2, :] = qout[2:-2, :] - (F_y[3:-2, :] - F_y[2:-3, :])/dy
 
-    # # with numexpr
-    # u22 = u[:, 2:-2]
-    # q21 = q[:, 2:-1]
-    # q12 = q[:, 1:-2]
-    # q3 = q[:, 3:]
-    # qn3 = q[:, :-3]
-    # F_x[:, 2:-2] = ne.evaluate('u22 / 12 * (7 * (q21 + q12) - (q3 + qn3))')
+#     # # with numexpr
+#     # u22 = u[:, 2:-2]
+#     # q21 = q[:, 2:-1]
+#     # q12 = q[:, 1:-2]
+#     # q3 = q[:, 3:]
+#     # qn3 = q[:, :-3]
+#     # F_x[:, 2:-2] = ne.evaluate('u22 / 12 * (7 * (q21 + q12) - (q3 + qn3))')
 
-    # v22 = v[2:-2, :]
-    # q21 = q[2:-1, :]
-    # q12 = q[1:-2, :]
-    # q3 = q[3:, :]
-    # qn3 = q[:-3, :]
-    # F_y[2:-2, :] = ne.evaluate('v22 / 12 * (7 * (q21 + q12) - (q3 + qn3))')
+#     # v22 = v[2:-2, :]
+#     # q21 = q[2:-1, :]
+#     # q12 = q[1:-2, :]
+#     # q3 = q[3:, :]
+#     # qn3 = q[:-3, :]
+#     # F_y[2:-2, :] = ne.evaluate('v22 / 12 * (7 * (q21 + q12) - (q3 + qn3))')
 
-    # qo22 = qout[:, 2:-2]
-    # fx32 = F_x[:, 3:-2]
-    # fx23 = F_x[:, 2:-3]
-    # qout[:, 2:-2] = ne.evaluate('qo22 - (fx32 - fx23) / dx')
+#     # qo22 = qout[:, 2:-2]
+#     # fx32 = F_x[:, 3:-2]
+#     # fx23 = F_x[:, 2:-3]
+#     # qout[:, 2:-2] = ne.evaluate('qo22 - (fx32 - fx23) / dx')
 
-    # qo22 = qout[2:-2, :]
-    # fy32 = F_y[3:-2, :]
-    # fy23 = F_y[2:-3, :]
-    # qout[2:-2, :] = ne.evaluate('qo22 - (fy32 - fy23) / dy')
+#     # qo22 = qout[2:-2, :]
+#     # fy32 = F_y[3:-2, :]
+#     # fy23 = F_y[2:-3, :]
+#     # qout[2:-2, :] = ne.evaluate('qo22 - (fy32 - fy23) / dy')
 
 
-    # boundary calculation
-    # u_w = u[:, 0:2].clip(max=0)
-    u_w = u[:, 0:2]
-    # u_w[u_w > 0] = 0
-    for j in range(u_w[0,:].size):
-        for i in range(u_w[:, 0].size):
-            if u_w[i, j] > 0:
-                u_w[i, j] = 0
-    # u_e = u[:, -2:].clip(min=0)
-    u_e = u[:, -2:]
-    # u_e[u_e < 0] = 0
-    for j in range(u_e[0,:].size):
-        for i in range(u_e[:, 0].size):
-            if u_e[i, j] < 0:
-                u_e[i, j] = 0
-    qout[:, 0:2] = qout[:, 0:2] - ((u_w/dx)*(
-        q[:, 1:3] - q[:, 0:2]) + (q[:, 0:2]/dx)*(u[:, 1:3] - u[:, 0:2]))
-    qout[:, -2:] = qout[:, -2:] - ((u_e/dx)*(
-       q[:, -2:] - q[:, -3:-1]) + (q[:, -2:]/dx)*(u[:, -2:] - u[:, -3:-1]))
+#     # boundary calculation
+#     # u_w = u[:, 0:2].clip(max=0)
+#     u_w = u[:, 0:2]
+#     # u_w[u_w > 0] = 0
+#     for j in range(u_w[0,:].size):
+#         for i in range(u_w[:, 0].size):
+#             if u_w[i, j] > 0:
+#                 u_w[i, j] = 0
+#     # u_e = u[:, -2:].clip(min=0)
+#     u_e = u[:, -2:]
+#     # u_e[u_e < 0] = 0
+#     for j in range(u_e[0,:].size):
+#         for i in range(u_e[:, 0].size):
+#             if u_e[i, j] < 0:
+#                 u_e[i, j] = 0
+#     qout[:, 0:2] = qout[:, 0:2] - ((u_w/dx)*(
+#         q[:, 1:3] - q[:, 0:2]) + (q[:, 0:2]/dx)*(u[:, 1:3] - u[:, 0:2]))
+#     qout[:, -2:] = qout[:, -2:] - ((u_e/dx)*(
+#        q[:, -2:] - q[:, -3:-1]) + (q[:, -2:]/dx)*(u[:, -2:] - u[:, -3:-1]))
 
-    # qo02 = qout[:, 0:2]
-    # q13 = q[:, 1:3]
-    # q02 = q[:, 0:2]
-    # u13 = u[:, 1:3]
-    # u02 = u[:, 0:2]
-    # qout[:, 0:2] = ne.evaluate('qo02 - ((u_w/dx)*(q13 - q02) + (q02/dx)*(u13 - u02))')
+#     # qo02 = qout[:, 0:2]
+#     # q13 = q[:, 1:3]
+#     # q02 = q[:, 0:2]
+#     # u13 = u[:, 1:3]
+#     # u02 = u[:, 0:2]
+#     # qout[:, 0:2] = ne.evaluate('qo02 - ((u_w/dx)*(q13 - q02) + (q02/dx)*(u13 - u02))')
 
-    # qo2 = qout[:, -2:]
-    # q2 = q[:, -2:]
-    # q31 = q[:, -3:-1]
-    # u2 = u[:, -2:]
-    # u31 = u[:, -3:-1]
-    # qout[:, -2:] = ne.evaluate('qo2 - ((u_e/dx)*(q2 - q31) + (q2/dx)*(u2 - u31))')
+#     # qo2 = qout[:, -2:]
+#     # q2 = q[:, -2:]
+#     # q31 = q[:, -3:-1]
+#     # u2 = u[:, -2:]
+#     # u31 = u[:, -3:-1]
+#     # qout[:, -2:] = ne.evaluate('qo2 - ((u_e/dx)*(q2 - q31) + (q2/dx)*(u2 - u31))')
 
-    # v_n = v[-2:, :].clip(min=0)
-    v_n = v[-2:, :]
-    # v_n[v_n < 0] = 0
-    for j in range(v_n[0,:].size):
-        for i in range(v_n[:, 0].size):
-            if v_n[i, j] < 0:
-                v_n[i, j] = 0
+#     # v_n = v[-2:, :].clip(min=0)
+#     v_n = v[-2:, :]
+#     # v_n[v_n < 0] = 0
+#     for j in range(v_n[0,:].size):
+#         for i in range(v_n[:, 0].size):
+#             if v_n[i, j] < 0:
+#                 v_n[i, j] = 0
     
-    # v_s = v[0:2, :].clip(max=0)
-    v_s = v[0:2, :]
-    # v_s[v_s > 0] = 0 
-    for j in range(v_s[0,:].size):
-        for i in range(v_s[:, 0].size):
-            if v_s[i, j] > 0:
-                v_s[i, j] = 0
-    qout[0:2, :] = qout[0:2, :] - ((v_s/dx)*(
-        q[1:3, :] - q[0:2, :]) + (q[0:2, :]/dx)*(v[1:3, :] - v[0:2, :]))
-    qout[-2:, :] = qout[-2:, :] - ((v_n/dx)*(
-        q[-2:, :] - q[-3:-1, :]) + (q[-2:, :]/dx)*(v[-2:, :] - v[-3:-1, :]))
+#     # v_s = v[0:2, :].clip(max=0)
+#     v_s = v[0:2, :]
+#     # v_s[v_s > 0] = 0 
+#     for j in range(v_s[0,:].size):
+#         for i in range(v_s[:, 0].size):
+#             if v_s[i, j] > 0:
+#                 v_s[i, j] = 0
+#     qout[0:2, :] = qout[0:2, :] - ((v_s/dx)*(
+#         q[1:3, :] - q[0:2, :]) + (q[0:2, :]/dx)*(v[1:3, :] - v[0:2, :]))
+#     qout[-2:, :] = qout[-2:, :] - ((v_n/dx)*(
+#         q[-2:, :] - q[-3:-1, :]) + (q[-2:, :]/dx)*(v[-2:, :] - v[-3:-1, :]))
 
-    # qo02 = qout[0:2, :]
-    # q13 = q[1:3, :]
-    # q02 = q[0:2, :]
-    # v13 = v[1:3, :]
-    # v02 = v[0:2, :]
-    # qout[0:2, :] = ne.evaluate('qo02 - ((v_s/dx)*(q13 - q02) + (q02/dx)*(v13 - v02))')
+#     # qo02 = qout[0:2, :]
+#     # q13 = q[1:3, :]
+#     # q02 = q[0:2, :]
+#     # v13 = v[1:3, :]
+#     # v02 = v[0:2, :]
+#     # qout[0:2, :] = ne.evaluate('qo02 - ((v_s/dx)*(q13 - q02) + (q02/dx)*(v13 - v02))')
 
-    # qo2 = qout[-2:, :]
-    # q2 = q[-2:, :]
-    # q31 = q[-3:-1, :]
-    # v2 = v[-2:, :]
-    # v31 = v[-3:-1, :]
-    # qout[-2:, :] = ne.evaluate('qo2 - ((v_n/dx)*(q2 - q31) + (q2/dx)*(v2 - v31))')
-    return qout
-### Numba test
+#     # qo2 = qout[-2:, :]
+#     # q2 = q[-2:, :]
+#     # q31 = q[-3:-1, :]
+#     # v2 = v[-2:, :]
+#     # v31 = v[-3:-1, :]
+#     # qout[-2:, :] = ne.evaluate('qo2 - ((v_n/dx)*(q2 - q31) + (q2/dx)*(v2 - v31))')
+#     return qout
+# ### Numba test
 
 def cot(theta):
     """Why doesn't numpy have cot?"""
