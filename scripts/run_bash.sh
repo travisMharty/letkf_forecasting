b0ca49d4a9aaf82c602b6b81cca6b3da54a708ab
travisMharty <travisharty@math.arizona.edu>
2018-03-08 09:51:47 -0700

    Parameters needed to be represented properly.
    Tell the config file that winds_sigma is a tuple and get rid of
    instances in which calculations are performed in the file.

--- scripts/run_bash.sh
+++ scripts/run_bash.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 year=2014
-declare -a month_day=("4 9" "4 15" "4 18" "5 6" "5 9" "5 29" "6 11" "6 12")
+# declare -a month_day=("4 9" "4 15" "4 18" "5 6" "5 9" "5 29" "6 11" "6 12")
+declare -a month_day=("4 15" "4 18" "5 6" "5 9" "5 29" "6 11" "6 12")
 
 for md in "${month_day[@]}"
 do
     date=($md)
     echo "${date[@]}"
-    python run_forecast_system.py config_wrf_mean.yml -y $year -m ${date[0]} -d ${date[1]}
+    python run_forecast_system.py config_no_sat2sat.yml -y $year -m ${date[0]} -d ${date[1]}
 done
