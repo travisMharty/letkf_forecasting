26290bbec166f9f1f41d5fc67b428cc7b2fa7a9f
travisMharty <travisharty@math.arizona.edu>
2018-03-09 09:39:33 -0700

    Added functions to improve analysis of data.
    return_day adds horizon coordinate so that analysis can be done based
    on forecast horizon. get_hoizon returns data from the horizon
    requested. return_error_domain return data only from error
    domain (only ci for now).

--- letkf_forecasting/analyse_results.py
+++ letkf_forecasting/analyse_results.py
@@ -1,6 +1,7 @@
 import os
 import xarray as xr
 import glob
+import numpy as np
 
 
 def return_day(year, month, day, run_name):
@@ -12,14 +13,37 @@ def return_day(year, month, day, run_name):
     paths.sort()
     path = paths[-1]
     path = os.path.join(path, '*.nc')
-    full_day = xr.open_mfdataset(path, concat_dim='init',
+    full_day = xr.open_mfdataset(path,
                                  preprocess=add_init,
                                  decode_cf=False)
-    full_day.init.attrs['units'] = full_day.time.units
+    full_day.horizon.attrs['units'] = 'minutes'
     full_day = xr.decode_cf(full_day)
     return full_day
 
 
 def add_init(ds):
-    ds.coords['init'] = ds.time.init
+    ds.coords['horizon'] = (ds.time - ds.time[0])/60
     return ds
+
+
+def get_horizon(df, horizon):
+    if type(horizon) != np.timedelta64:
+        horizon = np.timedelta64(horizon, 'm')
+    to_return = df.where(df.horizon == horizon, drop=True)
+    to_return = to_return.drop('horizon')
+    return to_return
+
+
+def return_error_domain(ds):
+    we_er_min = ds.we_er_min
+    we_er_max = ds.we_er_max
+    sn_er_min = ds.sn_er_min
+    sn_er_max = ds.sn_er_max
+    to_return = ds['ci'].sel(
+        west_east=slice(we_er_min, we_er_max),
+        south_north=slice(sn_er_min, sn_er_max))
+    return to_return
+
+
+def return_ens_mean(ds):
+    return ds.mean(dim='ensemble_number', keep_attrs=True)
