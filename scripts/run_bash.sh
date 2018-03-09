08324ff933e4fd8428dfddb3b4eb5e18371ce525
travisMharty <travisharty@math.arizona.edu>
2018-03-09 09:45:15 -0700

    Took away a date that I did not want to run.

--- scripts/run_bash.sh
+++ scripts/run_bash.sh
@@ -2,7 +2,7 @@
 
 year=2014
 # declare -a month_day=("4 9" "4 15" "4 18" "5 6" "5 9" "5 29" "6 11" "6 12")
-declare -a month_day=("4 15" "4 18" "5 6" "5 9" "5 29" "6 11" "6 12")
+declare -a month_day=("5 6" "5 9" "5 29" "6 11" "6 12")
 
 for md in "${month_day[@]}"
 do
