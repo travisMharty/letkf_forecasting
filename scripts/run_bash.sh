7c261f19bd1136487466326404b4f84a407d1d9f
travisMharty <travisharty@math.arizona.edu>
2018-05-14 08:08:24 -0700

    No major changes.

--- scripts/run_bash.sh
+++ scripts/run_bash.sh
@@ -3,7 +3,7 @@
 year=2014
 # declare -a month_day=("4 9" "4 15" "4 18" "5 6" "5 9" "5 29" "6 11" "6 12")
 # declare -a month_day=("5 6" "5 9" "5 29" "6 11" "6 12")
-declare -a month_day=("4 9" "4 15" "4 18" "5 6" "5 9")
+declare -a month_day=("6 11" "6 12")
 
 for md in "${month_day[@]}"
 do
