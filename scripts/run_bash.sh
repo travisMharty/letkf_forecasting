21c1161cfcba0213f24b4f8152509b9b86953099
travisMharty <travisharty@math.arizona.edu>
2018-05-15 17:34:08 -0700

    No major changes.

--- scripts/run_bash.sh
+++ scripts/run_bash.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 year=2014
-# declare -a month_day=("4 9" "4 15" "4 18" "5 6" "5 9" "5 29" "6 11" "6 12")
-declare -a month_day=("5 6" "5 9" "5 29" "6 11" "6 12")
-# declare -a month_day=("5 6" "5 9" "5 29" "6 11" "6 12")
+declare -a month_day=("4 9" "4 15" "4 18" "5 6" "5 9" "5 29" "6 11" "6 12")
 # declare -a month_day=("6 11" "6 12")
 
 for md in "${month_day[@]}"
 do
     date=($md)
     echo "${date[@]}"
-    python run_forecast_system.py config_radio.yml -y $year -m ${date[0]} -d ${date[1]}
+    python run_forecast_system.py config_wrf_no_div.yml -y $year -m ${date[0]} -d ${date[1]}
 done
