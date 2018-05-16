2fa77b94ae4cbf49b03c11faf3b6ba77c1636885
travisMharty <travisharty@math.arizona.edu>
2018-05-16 12:36:04 -0700

    Add multiple different configuration runs.

--- scripts/run_bash.sh
+++ scripts/run_bash.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 year=2014
-declare -a month_day=("4 9" "4 15" "4 18" "5 6" "5 9" "5 29" "6 11" "6 12")
+#declare -a month_day=("4 9" "4 15" "4 18" "5 6" "5 9" "5 29" "6 11" "6 12")
+declare -a month_day=("4 9" "5 9" "5 29" "6 11" "6 12")
 # declare -a month_day=("6 11" "6 12")
-# need to rerun ow_15 and ow_15_wp3 for 4 9
+
 for md in "${month_day[@]}"
 do
     date=($md)
@@ -19,7 +20,11 @@ do
     echo "ow_15"
     python run_forecast_system.py ow_15.yml -y $year -m ${date[0]} -d ${date[1]}
 
-    echo "${date[@]}"
-    echo "ow_15_wp3"
-    python run_forecast_system.py ow_15_wp3.yml -y $year -m ${date[0]} -d ${date[1]}
+    # echo "${date[@]}"
+    # echo "ow_15_wp3"
+    # python run_forecast_system.py ow_15_wp3.yml -y $year -m ${date[0]} -d ${date[1]}
+
+    # echo "${date[@]}"
+    # echo "opt_flow"
+    # python run_forecast_system.py config_opt_flow.yml -y $year -m ${date[0]} -d ${date[1]}
 done
