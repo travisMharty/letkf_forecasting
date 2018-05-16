5c7fd19c5664e808125ca33708b1a2e81182c633
travisMharty <travisharty@math.arizona.edu>
2018-05-15 17:41:37 -0700

    Run many configuration files on each of many days.

--- scripts/run_bash.sh
+++ scripts/run_bash.sh
@@ -8,5 +8,18 @@ for md in "${month_day[@]}"
 do
     date=($md)
     echo "${date[@]}"
+    echo "wrf_no_div"
     python run_forecast_system.py config_wrf_no_div.yml -y $year -m ${date[0]} -d ${date[1]}
+
+    echo "${date[@]}"
+    echo "radiosonde"
+    python run_forecast_system.py config_radio.yml -y $year -m ${date[0]} -d ${date[1]}
+
+    echo "${date[@]}"
+    echo "ow_15"
+    python run_forecast_system.py ow_15.yml -y $year -m ${date[0]} -d ${date[1]}
+
+    echo "${date[@]}"
+    echo "ow_15_wp3"
+    python run_forecast_system.py ow_15_wp3.yml -y $year -m ${date[0]} -d ${date[1]}
 done
