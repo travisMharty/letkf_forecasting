5f133bfd0a8aa9cc5c2c737ad14d0f2214686db8
travisMharty <travisharty@math.arizona.edu>
2018-05-25 09:09:02 -0700

    This script runs many days through run_forecast_system.py

---
+++ scripts/run_many_days.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+year=2014
+# declare -a month_day=("4 9" "4 15" "4 18" "5 6" "5 9" "5 29" "6 11" "6 12")
+declare -a month_day=("4 9" "5 6" "5 9" "5 29" "6 11" "6 12")
+# declare -a month_day=("5 19" "6 03" "6 10" "6 14" "6 15")
+
+for md in "${month_day[@]}"
+do
+    date=($md)
+    # echo "${date[@]}"
+    # echo "wrf_no_div"
+    # python run_forecast_system.py config_wrf_no_div.yml -y $year -m ${date[0]} -d ${date[1]}
+
+    # echo "${date[@]}"
+    # echo "radiosonde"
+    # python run_forecast_system.py config_radio.yml -y $year -m ${date[0]} -d ${date[1]}
+
+    # echo "${date[@]}"
+    # echo "ow_15"
+    # python run_forecast_system.py ow_15.yml -y $year -m ${date[0]} -d ${date[1]}
+
+    echo "${date[@]}"
+    echo "ow_15_wp3"
+    python run_forecast_system.py ow_15_wp3.yml -y $year -m ${date[0]} -d ${date[1]}
+done
