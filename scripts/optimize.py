071fc8cf37c2225f88a67687a5f7091e99636eb0
travisMharty <travisharty@math.arizona.edu>
2018-05-30 15:24:17 -0700

    Add workers parameter, break off functions, calculate error.

--- scripts/optimize.py
+++ scripts/optimize.py
@@ -2,12 +2,66 @@ import argparse
 import yaml
 import os
 import logging
+import multiprocessing
 import importlib
 import time as time_py
 import letkf_forecasting.letkf_forecasting as lf
 import letkf_forecasting.analyse_results as analyse_results
 from letkf_forecasting import __version__
 import numpy as np
+import pandas as pd
+
+
+def set_up_configuration(args, l_w, i_w, l_of, i_of, sig_pw, l_pw):
+    with open(args.file_path, 'r') as ymlfile:
+        cfg = yaml.load(ymlfile)
+    if args.year is not None:
+        cfg['date']['year'] = args.year
+    if args.month is not None:
+        cfg['date']['month'] = args.month
+    if args.day is not None:
+        cfg['date']['day'] = args.day
+    i_w_str = str(i_w).replace('.', 'p')
+    i_of_str = str(i_of).replace('.', 'p')
+    sig_pw_str = str(sig_pw).replace('.', 'p')
+    cfg['wrf']['loc'] = l_w
+    cfg['wrf']['infl'] = i_w
+    cfg['opt_flow']['loc'] = l_of
+    cfg['opt_flow']['infl'] = i_of
+    opt_str = (f'_{l_w:03}_{i_w_str}_{l_of:03}_'
+               + f'{i_of_str}_{l_pw:03}_{sig_pw_str}')
+    cfg['io']['run_name'] = (
+        cfg['io']['run_name'] + opt_str)
+    cfg['version'] = __version__
+    return cfg
+
+
+def set_up_logging(results_folder_path):
+    log_path = os.path.join(results_folder_path,
+                            'forecast.log')
+    logging.shutdown()
+    importlib.reload(logging)
+    logging.basicConfig(
+        filename=log_path,
+        format='%(asctime)s %(levelname)s %(name)s %(message)s',
+        filemode='w', level=logging.DEBUG)
+
+
+def error_analysis(cfg, results_folder_path, home, optimize_folder):
+    year = cfg['date']['year']
+    month = cfg['date']['month']
+    day = cfg['date']['day']
+    run = cfg['io']['run_name']
+    returned = analyse_results.error_stats(
+        year, month, day, [run], home, optimize_folder)
+    for k, v in returned[0].items():
+        conditional = (isinstance(v, type(pd.Series()))
+                       or isinstance(v, type(pd.DataFrame())))
+        if conditional:
+            file_path = os.path.join(
+                results_folder_path,
+                f'{k}.h5')
+            v.to_hdf(file_path, k)
 
 
 def main():
@@ -21,12 +75,16 @@ def main():
                         help='The month you wish to run.')
     parser.add_argument('-d', '--day', type=int,
                         help='The day you wish to run.')
+    parser.add_argument('--home', help='base directory for data and results',
+                        default='/a2/uaren/travis/')
+    parser.add_argument('--workers', help='Number of advection workers',
+                        default=20, type=int)
     args = parser.parse_args()
 
     # wrf_length_array = np.array([4, 10])
     wrf_length_array = np.array([4, 10])
     of_length_array = np.array([4, 20])
-    wrf_inflation_array = np.array([1.5, 5])
+    wrf_inflation_array = np.array([1.5, 4])
     of_inflation_array = np.array([1.5, 2])
     sig_pw = 0
     l_pw = 0
@@ -34,25 +92,8 @@ def main():
         for i_w in wrf_inflation_array:
             for l_of in of_length_array:
                 for i_of in of_inflation_array:
-                    with open(args.file_path, 'r') as ymlfile:
-                        cfg = yaml.load(ymlfile)
-                    if args.year is not None:
-                        cfg['date']['year'] = args.year
-                    if args.month is not None:
-                        cfg['date']['month'] = args.month
-                    if args.day is not None:
-                        cfg['date']['day'] = args.day
-                    i_w_str = str(i_w).replace('.', 'p')
-                    i_of_str = str(i_of).replace('.', 'p')
-                    sig_pw_str = str(sig_pw).replace('.', 'p')
-                    cfg['wrf']['loc'] = l_w
-                    cfg['wrf']['infl'] = i_w
-                    cfg['opt_flow']['loc'] = l_of
-                    cfg['opt_flow']['infl'] = i_of
-                    opt_str = (f'_{l_w:03}_{i_w_str}_{l_of:03}_'
-                               + f'{i_of_str}_{l_pw:03}_{sig_pw_str}')
-                    cfg['io']['run_name'] = (
-                        cfg['io']['run_name'] + opt_str)
+                    cfg = set_up_configuration(args, l_w, i_w,
+                                               l_of, i_of, sig_pw, l_pw)
                     print(cfg['io']['run_name'])
                     year = cfg['date']['year']
                     month = cfg['date']['month']
@@ -65,16 +106,10 @@ def main():
                         cfg['io']['run_name'])
                     if not os.path.exists(results_folder_path):
                         os.makedirs(results_folder_path)
-                    # # Create path to save results
-                    # results_file_path = letkf_io.create_folder(
-                    #     cfg['date']['year'],
-                    #     cfg['date']['month'],
-                    #     cfg['date']['day'],
-                    #     cfg['io']['run_name'])
                     yaml_file_path = os.path.join(
                         results_folder_path,
                         'config_' + cfg['io']['run_name'] + '.yml')
-                    cfg['version'] = __version__
+
                     with open(yaml_file_path, 'w') as ymlfile:
                         yaml.dump(cfg, ymlfile, default_flow_style=False)
                     home = '/a2/uaren/travis/'
@@ -82,15 +117,16 @@ def main():
                         home=home, year=cfg['date']['year'],
                         month=cfg['date']['month'], day=cfg['date']['day'])
                     time0 = time_py.time()
-
-                    log_path = os.path.join(results_folder_path,
-                                            'forecast.log')
-                    logging.shutdown()
-                    importlib.reload(logging)
-                    logging.basicConfig(
-                        filename=log_path,
-                        filemode='w', level=logging.DEBUG)
+                    set_up_logging(results_folder_path)
                     logging.info('Started')
+                    # use less memory than fork
+                    try:
+                        multiprocessing.set_start_method('forkserver')
+                    except RuntimeError:
+                        pass
+                    workers = (cfg['advect_params']['workers']
+                               if 'workers' in cfg['advect_params']
+                               else args.workers)
                     try:
                         lf.forecast_system(
                             data_file_path=data_file_path,
@@ -102,10 +138,18 @@ def main():
                             sat2sat=cfg['sat2sat'],
                             sat2wind=cfg['sat2wind'],
                             wrf=cfg['wrf'],
-                            opt_flow=cfg['opt_flow'])
+                            opt_flow=cfg['opt_flow'],
+                            workers=workers)
+                        logging.info('forecast_system ended')
                     except Exception:
                         logging.exception('forecast_system failed')
-                    logging.info('Ended')
+                    logging.info('begin error analysis')
+                    try:
+                        error_analysis(cfg, results_folder_path,
+                                       home, 'results_opt')
+                        logging.inlfo('error analysis ended')
+                    except Exception:
+                        logging.exception('error_analysis failed')
                     time1 = time_py.time()
                     print('It took: ' + str((time1 - time0)/60))
                     logging.info('It took: ' + str((time1 - time0)/60))
