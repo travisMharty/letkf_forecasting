059f8068a5ae39cf046471a6c6997d8998e4fb71
travisMharty <travisharty@math.arizona.edu>
2018-08-06 15:02:42 -0700

    No major changes.

--- scripts/run_calc_error.sh
+++ scripts/run_calc_error.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 year=2014
-declare -a month_day=("4 9" "4 15" "4 18" "5 6" "5 9" "5 29" "6 11" "6 12")
-
+# declare -a month_day=("4 9" "4 15" "4 18" "5 6" "5 9" "5 29" "6 11" "6 12")
 # declare -a month_day=("4 2" "4 5" "4 19" "5 7" "5 8" "5 19" "6 03" "6 10" "6 14" "6 15")
+declare -a month_day=("4 9" "4 15" "4 18" "5 6" "5 9" "5 29" "6 11" "6 12" "4 2" "4 5" "4 19" "5 7" "5 8" "5 19" "6 03" "6 10" "6 14" "6 15")
 
 for md in "${month_day[@]}"
 do
@@ -28,13 +28,13 @@ do
     # echo "config_persistence"
     # python calc_error.py config_persistence.yml -y $year -m ${date[0]} -d ${date[1]}
 
-    echo "${date[@]}"
-    echo "ow_15_wp3"
-    python calc_error.py ow_15_wp3.yml -y $year -m ${date[0]} -d ${date[1]}
-
     # echo "${date[@]}"
-    # echo "ow_15"
-    # python calc_error.py ow_15.yml -y $year -m ${date[0]} -d ${date[1]}
+    # echo "ow_15_wp3"
+    # python calc_error.py ow_15_wp3.yml -y $year -m ${date[0]} -d ${date[1]}
+
+    echo "${date[@]}"
+    echo "ow_15"
+    python calc_error.py ow_15.yml -y $year -m ${date[0]} -d ${date[1]}
 
     # echo "${date[@]}"
     # echo "owp_opt"
