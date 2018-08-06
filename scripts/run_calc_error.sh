b034987a7a9696b3de7bd94c5df4033bfb85476b
travisMharty <travisharty@math.arizona.edu>
2018-08-06 08:54:22 -0700

    No major changes.

--- scripts/run_calc_error.sh
+++ scripts/run_calc_error.sh
@@ -2,6 +2,7 @@
 
 year=2014
 declare -a month_day=("4 9" "4 15" "4 18" "5 6" "5 9" "5 29" "6 11" "6 12")
+
 # declare -a month_day=("4 2" "4 5" "4 19" "5 7" "5 8" "5 19" "6 03" "6 10" "6 14" "6 15")
 
 for md in "${month_day[@]}"
@@ -23,7 +24,19 @@ do
     # echo "config_opt_flow"
     # python calc_error.py config_opt_flow.yml -y $year -m ${date[0]} -d ${date[1]}
 
+    # echo "${date[@]}"
+    # echo "config_persistence"
+    # python calc_error.py config_persistence.yml -y $year -m ${date[0]} -d ${date[1]}
+
+    # echo "${date[@]}"
+    # echo "ow_15_wp3"
+    # python calc_error.py ow_15_wp3.yml -y $year -m ${date[0]} -d ${date[1]}
+
+    # echo "${date[@]}"
+    # echo "ow_15"
+    # python calc_error.py ow_15.yml -y $year -m ${date[0]} -d ${date[1]}
+
     echo "${date[@]}"
-    echo "config_persistence"
-    python calc_error.py config_persistence.yml -y $year -m ${date[0]} -d ${date[1]}
+    echo "owp_opt"
+    python calc_error.py owp_opt.yml -y $year -m ${date[0]} -d ${date[1]}
 done
--- scripts/run_many_days.sh
+++ scripts/run_many_days.sh
@@ -2,7 +2,7 @@
 
 year=2014
 # declare -a month_day=("4 9" "4 15" "4 18" "5 6" "5 9" "5 29" "6 11" "6 12")
-# declare -a month_day=("4 9" "5 6" "5 9" "5 29" "6 11" "6 12")
+
 declare -a month_day=("4 2" "4 5" "4 19" "5 7" "5 8" "5 19" "6 03" "6 10" "6 14" "6 15")
 
 for md in "${month_day[@]}"
@@ -24,7 +24,11 @@ do
     # echo "ow_15_wp3"
     # python run_forecast_system.py ow_15_wp3.yml -y $year -m ${date[0]} -d ${date[1]}
 
+    # echo "${date[@]}"
+    # echo "config_wrf_mean.yml"
+    # python run_forecast_system.py config_wrf_mean.yml -y $year -m ${date[0]} -d ${date[1]}
+
     echo "${date[@]}"
-    echo "config_wrf_mean.yml"
-    python run_forecast_system.py config_wrf_mean.yml -y $year -m ${date[0]} -d ${date[1]}
+    echo "config_owp_opt.yml"
+    python run_forecast_system.py config_owp_opt.yml -y $year -m ${date[0]} -d ${date[1]}
 done
