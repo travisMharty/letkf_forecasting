d6c460c3754d41c03b3c1499bc9e20ff80be230a
travisMharty <travisharty@math.arizona.edu>
2018-08-07 14:19:18 -0700

    No major changes.

--- scripts/run_calc_error.sh
+++ scripts/run_calc_error.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 year=2014
-# declare -a month_day=("4 9" "4 15" "4 18" "5 6" "5 9" "5 29" "6 11" "6 12")
+declare -a month_day=("4 9" "4 15" "4 18" "5 6" "5 9" "5 29" "6 11" "6 12")
 # declare -a month_day=("4 2" "4 5" "4 19" "5 7" "5 8" "5 19" "6 03" "6 10" "6 14" "6 15")
-declare -a month_day=("4 9" "4 15" "4 18" "5 6" "5 9" "5 29" "6 11" "6 12" "4 2" "4 5" "4 19" "5 7" "5 8" "5 19" "6 03" "6 10" "6 14" "6 15")
+#declare -a month_day=("4 9" "4 15" "4 18" "5 6" "5 9" "5 29" "6 11" "6 12" "4 2" "4 5" "4 19" "5 7" "5 8" "5 19" "6 03" "6 10" "6 14" "6 15")
 
 for md in "${month_day[@]}"
 do
@@ -11,6 +11,14 @@ do
     # echo "${date[@]}"
     # echo "wrf_no_div"
     # python calc_error.py config_wrf_no_div.yml -y $year -m ${date[0]} -d ${date[1]}
+
+    # echo "${date[@]}"
+    # echo "wrf"
+    # python calc_error.py config_wrf.yml -y $year -m ${date[0]} -d ${date[1]}
+
+    echo "${date[@]}"
+    echo "opt_flow"
+    python calc_error.py config_opt_flow.yml -y $year -m ${date[0]} -d ${date[1]}
 
     # echo "${date[@]}"
     # echo "radiosonde"
@@ -32,9 +40,9 @@ do
     # echo "ow_15_wp3"
     # python calc_error.py ow_15_wp3.yml -y $year -m ${date[0]} -d ${date[1]}
 
-    echo "${date[@]}"
-    echo "ow_15"
-    python calc_error.py ow_15.yml -y $year -m ${date[0]} -d ${date[1]}
+    # echo "${date[@]}"
+    # echo "ow_15"
+    # python calc_error.py ow_15.yml -y $year -m ${date[0]} -d ${date[1]}
 
     # echo "${date[@]}"
     # echo "owp_opt"
--- scripts/run_many_days.sh
+++ scripts/run_many_days.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 year=2014
-# declare -a month_day=("4 9" "4 15" "4 18" "5 6" "5 9" "5 29" "6 11" "6 12")
+declare -a month_day=("4 9" "4 15" "4 18" "5 6" "5 9" "5 29" "6 11" "6 12")
+# declare -a month_day=("4 2" "4 5" "4 19" "5 7" "5 8" "5 19" "6 03" "6 10" "6 14" "6 15")
+# declare -a month_day=("4 9" "4 15" "4 18" "5 6" "5 9" "5 29" "6 11" "6 12" "4 2" "4 5" "4 19" "5 7" "5 8" "5 19" "6 03" "6 10" "6 14" "6 15")
 
-declare -a month_day=("4 2" "4 5" "4 19" "5 7" "5 8" "5 19" "6 03" "6 10" "6 14" "6 15")
 
 for md in "${month_day[@]}"
 do
     date=($md)
+    # echo "${date[@]}"
+    # echo "wrf"
+    # python run_forecast_system.py config_wrf.yml -y $year -m ${date[0]} -d ${date[1]}
+
+    echo "${date[@]}"
+    echo "opt_flow"
+    python run_forecast_system.py config_opt_flow.yml -y $year -m ${date[0]} -d ${date[1]}
+
     # echo "${date[@]}"
     # echo "wrf_no_div"
     # python run_forecast_system.py config_wrf_no_div.yml -y $year -m ${date[0]} -d ${date[1]}
@@ -28,7 +37,7 @@ do
     # echo "config_wrf_mean.yml"
     # python run_forecast_system.py config_wrf_mean.yml -y $year -m ${date[0]} -d ${date[1]}
 
-    echo "${date[@]}"
-    echo "config_owp_opt.yml"
-    python run_forecast_system.py config_owp_opt.yml -y $year -m ${date[0]} -d ${date[1]}
+    # echo "${date[@]}"
+    # echo "config_owp_opt.yml"
+    # python run_forecast_system.py config_owp_opt.yml -y $year -m ${date[0]} -d ${date[1]}
 done
